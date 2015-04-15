902cc9348a7f8a11f0b7fefff39bae89b72bb426
sherrylml <sherrylml@126.com>
2015-04-15 20:30:50 +0800

    connect with mysql

--- labsite/bigdata/models.py
+++ labsite/bigdata/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Members(models.Model):
+    name = models.CharField(max_length=30)
+    identity = models.CharField(max_length=30)
+    homepage = models.URLField(null=True)
+    def __str__(self):
+        return self.name
