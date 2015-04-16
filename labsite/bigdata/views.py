69267fefe6e5ed0601adc68f335f91b4976e4c78
sherrylml <sherrylml@126.com>
2015-04-16 11:38:31 +0800

    templates and static files

--- labsite/bigdata/views.py
+++ labsite/bigdata/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    context = {}
+    return render(request, 'bigdata/index.html', context)
