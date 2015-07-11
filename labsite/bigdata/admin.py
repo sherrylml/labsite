fefdeca7132fdfa272e765c933a952bac7b605dd
pipilove <pipisorry@126.com>
2015-07-11 23:55:40 +0800

    changes

--- labsite/bigdata/admin.py
+++ labsite/bigdata/admin.py
@@ -11,27 +11,35 @@ from .models.team_models import *
 
 class NewsAdmin(admin.ModelAdmin):
     list_display = ['title', 'time_stamp']
+
+
 admin.site.register(News, NewsAdmin)
 # admin.site.register(News)
 
 class NoticesAdmin(admin.ModelAdmin):
     list_display = ['title', 'time_stamp']
+
+
 admin.site.register(Notices, NewsAdmin)
 
 '''
 Team
 '''
 # class ResDirInline(admin.StackedInline):
-class ResDirInline(admin.TabularInline):#要重启
+class ResDirInline(admin.TabularInline):  # 要重启
     model = ResDir
     extra = 1
+
+
 class ProfessorAdmin(admin.ModelAdmin):
     inlines = [ResDirInline]
+
+
 admin.site.register(Professor, ProfessorAdmin)
 
 
 # for class_i in [WorkExp, ResDir, PatPri, PubInf, ResAct]:
-#     admin.site.register(class_i)
+# admin.site.register(class_i)
 
 for class_i in [Postgraduate]:
     admin.site.register(class_i)
