9aabc4031caba4ee74869433441a46778c23df33
sherrylml <sherrylml@126.com>
2015-07-11 20:05:10 +0800

    change html files and models...

--- labsite/bigdata/urls.py
+++ labsite/bigdata/urls.py
@@ -5,7 +5,12 @@ from bigdata import views
 urlpatterns = patterns('',
         url(r'^index$',  views.index, name='index'),
         url(r'^news$', views.news, name='news'),
-        url(r'^team$', views.team, name='team')
+        url(r'^team$', views.team, name='team'),
+        url(r'^research$', views.research, name='research'),
+        url(r'^academic$', views.academic, name='academic'),
+        url(r'^relax$', views.relax, name='relax'),
+        url(r'^join$', views.join, name='join'),
+        url(r'^about$', views.about, name='about'),
     )
 
 
--- labsite/bigdata/views.py
+++ labsite/bigdata/views.py
@@ -5,6 +5,9 @@ from .models.team_models import Professor, Postgraduate
 
 
 def index(request):
+    '''
+    首页
+    '''
     Month = {1:'JAN', 2:'FEB',3:'MAR', 4:'APR', 5:'MAY',6:'JUN',7:'JUL',8:'AUG',9:'SEP',10:'OCT',11:'NOV',12:'DEC'}
     news_list = News.objects.all().order_by('-time_stamp')[0:4]
     notices_list = Notices.objects.all().order_by('-time_stamp')[0:4]
@@ -13,14 +16,56 @@ def index(request):
 
 
 def news(request):
-    newss = News.objects.order_by('-time_stamp')
-    context = {'newss': newss}
-    return render_to_response('bigdata/news-pi.html', context)
+    '''
+    通知&新闻
+    '''
+    news_list = News.objects.order_by('-time_stamp')
+    Context = {'news_list':news_list}
+    return render_to_response('bigdata/news.html', Context)
 
 
 def team(request):
+    '''
+    科研团队
+    '''
     professors = Professor.objects.all()
     masters = Postgraduate.objects.filter()
     doctors = Postgraduate.objects.filter()
-    return render_to_response('bigdata/team-pi.html', locals())
+    return render_to_response('bigdata/team.html', locals())
+
+def research(request):
+    '''
+    科学研究
+    '''
+    Context = {}
+    return render_to_response('bigdata/research.html', Context)
+
+def academic(request):
+    '''
+    合作交流
+    '''
+    Context = {}
+    return render_to_response('bigdata/academic.html', Context)
+
+def relax(request):
+    '''
+    活动休闲
+    '''
+    Context = {}
+    return render_to_response('bigdata/relax.html', Context)
+
+def join(request):
+    '''
+    加入我们
+    '''
+    Context = {}
+    return render_to_response('bigdata/join.html', Context)
+
+def about(request):
+    '''
+    关于
+    '''
+    Context = {}
+    return render_to_response('bigdata/about.html', Context)
+
 
