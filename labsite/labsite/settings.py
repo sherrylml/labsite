f686374d6c35225c694e7b7e70347069fe0a0324
sherrylml <sherrylml@126.com>
2015-07-12 19:49:46 +0800

    show all pages

--- labsite/labsite/settings.py
+++ labsite/labsite/settings.py
@@ -90,7 +90,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/upload/'
-# MEDIA_ROOT = (
-#     os.path.join(BASE_DIR,'upload'),
-# )
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'bigdata/upload')
