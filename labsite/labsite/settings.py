08934c8b4a94bc6934b8a81d0b083033cb0c1a5b
sherrylml <sherrylml@126.com>
2015-08-23 00:32:01 +0800

    Merge branch 'dev' of github.com:sherrylml/labsite into dev

--- labsite/labsite/settings.py
+++ labsite/labsite/settings.py
@@ -1,8 +1,114 @@
-# -*- coding: utf-8 -*-
+"""
+Django settings for labsite project.
 
-# Form implementation generated from reading ui file 'settings.py'
-#
-# Created by: PyQt5 UI code generator 5.4.1
-#
-# WARNING! All changes made in this file will be lost!
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
 
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+from django.utils.translation import ugettext_lazy as _
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+# E:/mine/python_workspace/WebSite/labsite/labsite
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '$6&e5xiotksif&#*!muyupw2oht+p&%+&c5)(n--gxw3n%lx$_'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = ['127.0.0.1']
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'bigdata',
+    'nested_inline'
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.i18n',
+    'django.contrib.auth.context_processors.auth',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'labsite.urls'
+
+WSGI_APPLICATION = 'labsite.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lab_data',
+        'USER': 'root',
+        'PASSWORD': '1994419',
+        # 'USER': 'pipi',
+        # 'PASSWORD': '249784435',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGES = (
+    # ('de', 'Deutsch'),
+    ('en', 'English'),
+    ('zh', '简体中文'),
+    # ('zh-cn', '简体中文'),
+)
+LOCALE_PATHS = '/locale/'
+
+LANGUAGE_CODE = 'en'
+# LANGUAGE_CODE = 'zh-cn'
+
+TIME_ZONE = 'utc'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
+
+# STATIC_ROOT = os.path.join(BASE_DIR, "bigdata/static")
+
+MEDIA_URL = '/upload/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'bigdata/upload')
